07e2cd9cb035b41b9b8c1987ac7c5c6b5d7bc00c
Jacob Cluff <djapanman@Jacobs-MacBook-Pro.local>
2024-06-22 23:32:17 -0700

    updated set_toolbox to allow zsh

--- set_toolbox.sh
+++ set_toolbox.sh
@@ -1,6 +1,13 @@
 if [ -z "$DIR_TOOLBOX" ]; then
 
-    declare -x DIR_TOOLBOX=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+    if [[ "$SHELL" = *"zsh"* ]]; then
+        echo "FOUND: zsh"
+        declare -x DIR_TOOLBOX=${0:a:h}
+    else
+        echo "FOUND: bash"
+        declare -x DIR_TOOLBOX=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+    fi
+
     echo "set DIR_TOOLBOX to $DIR_TOOLBOX"
 
     declare -x PYTHONPATH=$DIR_TOOLBOX
